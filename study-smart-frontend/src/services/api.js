d46c2c14663990c35979adc4e599f9495e0cf678
Arleen Kaur <arleenkaurarora9@gmail.com>
2025-07-30 00:50:41 -0700

    Hardcode backend URL for testing

--- study-smart-frontend/src/services/api.js
+++ study-smart-frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "https://study-smart-4ezm.onrender.com";
 
 export async function signup(username, password) {
   await fetch(`${BASE_URL}/auth/register`, {
